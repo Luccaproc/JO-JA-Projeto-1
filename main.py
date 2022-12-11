d7e863fcaea8ab98d9a3d3d3e08c137175688c56
Luccaproc <lucca.procopio@hotmail.com>
2022-12-11 18:00:50 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame, sys
+from settings import *
+from level import Level
+
+pygame.init()
+
+screen = pygame.display.set_mode((screen_width,screen_height))
+clock = pygame.time.Clock()
+level = Level(level_map,screen)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((20,0,60)) 
+    level.run()
+
+    pygame.display.update()
+    clock.tick(60)
+
+
---
+++ settings.py
@@ -0,0 +1,15 @@
+level_map = [
+'                               ',
+'                   XX          ',
+'          XXXX     XX      XXXX',
+'           XX      XX          ',
+'XXXXXX             XX          ',
+'XXX                XXXXXX      ',
+'                   XX          ',
+'        XXXX       XX          ',
+'        XXXX       XX          ',
+'XXXXXXXXXXXX       XXXXXXXXXXXX']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
